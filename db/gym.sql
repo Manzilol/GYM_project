0c4b7dc981e2de2689966eb5da57177ddbabd3e7
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-21 10:24:12 +0100

    Starting sunday, Database has tables. populating database next

--- db/gym.sql
+++ db/gym.sql
@@ -1,11 +1,10 @@
-DROP TABLE sessions;
-DROP TABLE members;
-DROP TABLE bookings;
-
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS sessions;
 
 
 CREATE TABLE sessions (
-    session_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     room VARCHAR(255),
     duration INT,
@@ -14,17 +13,16 @@ CREATE TABLE sessions (
 );
 
 CREATE TABLE members (
-    member_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     age INT,
-    sex VARCHAR(255),
-    booking_id INT NOT NULL REFERENCES bookings(id)
+    sex VARCHAR(255)
 );
 
 CREATE TABLE bookings (
-    booking_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL  REFERENCES members(id) ON DELETE CASCADE,
-    session_id INT NOT NULL  REFERENCES sessions(id) ON DELETE CASCADE,
+    id SERIAL PRIMARY KEY,
+    member_id INT NOT NULL REFERENCES members(id) ON DELETE CASCADE,
+    session_id INT NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
     notes VARCHAR(255)
 );
 

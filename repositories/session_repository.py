2b63791b331edb42fc3a3b7ca8863756be326d6d
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-21 10:57:33 +0100

    syntyax bug, possible typo on RETURNING

--- repositories/session_repository.py
+++ repositories/session_repository.py
@@ -4,7 +4,7 @@ from db.run_sql import run_sql
 from models.session import Session
 
 def save(session):
-    sql = "INSERT INTO sessions(name, room, duration, capacity, difficulty) VALUES (%s, %s, %s, %s, %s) RETURING id"
+    sql = "INSERT INTO sessions(name, room, duration, capacity, difficulty) VALUES (%s, %s, %s, %s, %s) RETURNING id"
     values = [session.name, session.room, session.duration, session.capacity, session.difficulty]
     results = run_sql(sql, values)
     session.id = results[0]['id']

576fdefe0d433bdddde6bbdc8344280a4a812505
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-21 10:46:38 +0100

    extra comma on values entry in repository possible bug.

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -3,7 +3,7 @@ from db.run_sql import run_sql
 from models.member import Member
 
 def save(member):
-    sql = "INSERT INTO members(name, age, sex) VALUES (%s, %s, %s,) RETURNING id"
+    sql = "INSERT INTO members(name, age, sex) VALUES (%s, %s, %s) RETURNING id"
     values = [member.name, member.age, member.sex]
     results = run_sql(sql, values)
     member.id = results[0]['id']

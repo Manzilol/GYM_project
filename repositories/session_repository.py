77ac8b6141c8016e597aa9487c472a2a72a85c10
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-21 10:56:11 +0100

    coded session repository, will run next to check it populates database.

---
+++ repositories/session_repository.py
@@ -0,0 +1,11 @@
+from cProfile import run
+from db.run_sql import run_sql
+
+from models.session import Session
+
+def save(session):
+    sql = "INSERT INTO sessions(name, room, duration, capacity, difficulty) VALUES (%s, %s, %s, %s, %s) RETURING id"
+    values = [session.name, session.room, session.duration, session.capacity, session.difficulty]
+    results = run_sql(sql, values)
+    session.id = results[0]['id']
+    return session

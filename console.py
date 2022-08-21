0e45e4ccd6aaca9579121b3aaa8ead64e00cde2c
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-21 10:38:12 +0100

    errors with console due to mismatched parameters

--- console.py
+++ console.py
@@ -7,20 +7,17 @@ import repositories.booking_repository as booking_repository
 import repositories.member_repository as member_repository
 import repositories.session_repository as session_repository
 
-booking_repository.delete_all()
-member_repository.delete_all()
-session_repository.delete_all()
+# booking_repository.delete_all()
+# member_repository.delete_all()
+# session_repository.delete_all()
 
-member1 = Member('Chuck Norris', 17, "male", 1)
+member1 = Member('Chuck Norris', 17, "male")
 member_repository.save(member1)
 
-member2 = Member('Mr T', 25, "male", 2)
+member2 = Member('Mr T', 25, "male")
 member_repository.save(member1)
 
-member1 = Member('Samus Aran', 23, "female", 4)
+member1 = Member('Samus Aran', 23, "female")
 member_repository.save(member1)
-
-
-# loc = visit_repository.location(visit4)
 
 # pdb.set_trace()

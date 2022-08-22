a30d874a0493e66e13c6e8a23d8a8c363d816884
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-22 11:01:39 +0100

    website runs. Looks like the homepage is doing well. taking break.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-from controllers.booking_controller import booking_blueprint
-from controllers.member_controller import member_blueprint
-from controllers.session_controller import session_blueprint
+# from controllers.booking_controller import booking_blueprint
+# from controllers.member_controller import member_blueprint
+# from controllers.session_controller import session_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(booking_blueprint)
-app.register_blueprint(member_blueprint)
-app.register_blueprint(session_blueprint)
+# app.register_blueprint(booking_blueprint)
+# app.register_blueprint(member_blueprint)
+# app.register_blueprint(session_blueprint)
 
 @app.route('/')
 def home():

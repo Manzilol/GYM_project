8b172c2f697d28c713037aedacc50a1479df8d35
Stewart Manzi <stewartmanzi@gmail.com>
2022-08-22 14:06:47 +0100

    change of direction. added the delete button funcitonality. Now all the actions are done. Now adding this to the other tables

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -36,3 +36,8 @@ def update_member(id):
     member = Member(name, age, sex, id)
     member_repository.update(member)
     return redirect("/members")
+
+@members_blueprint.route("/members/<id>/delete", methods=["POST"])
+def delete_member(id):
+    member_repository.delete(id)
+    return redirect("/members")
